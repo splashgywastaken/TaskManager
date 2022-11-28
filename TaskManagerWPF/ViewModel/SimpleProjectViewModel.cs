13c26d6ffaa0cb64f6b68678b6ed5fc06db524ee
Aleksey <38614548+splashgywastaken@users.noreply.github.com>
2022-11-28 22:42:14 +0300

    File changes, created new view
    Made so that styles and everything fill be in separate .xaml file
    Created new basic view for project

---
+++ TaskManagerWPF/ViewModel/SimpleProjectViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManagerWPF.ViewModel;
+
+public class SimpleProjectViewModel
+{
+    public string ProjectName { get; set; } = "Sample project name";
+    public string ProjectDescription { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ";
+}
