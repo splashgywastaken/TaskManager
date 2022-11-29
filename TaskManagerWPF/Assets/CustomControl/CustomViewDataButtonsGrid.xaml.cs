ecd35037313431c9496ea700c98f84a88953224d
Aleksey <38614548+splashgywastaken@users.noreply.github.com>
2022-11-29 22:51:00 +0300

    Made base for showing of projects
    Added some buttons and styles to SimpleProjectView view
    Made so that you can see your projects in ProjectsView view
    Basicly made some base to see what projects you have so that I could make them display projects that I need in the future

---
+++ TaskManagerWPF/Assets/CustomControl/CustomViewDataButtonsGrid.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManagerWPF.Assets.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for CustomViewDataButtonsGrid.xaml
+    /// </summary>
+    public partial class CustomViewDataButtonsGrid : UserControl
+    {
+        public static readonly DependencyProperty FirstButtonContentProperty = DependencyProperty.Register(
+            nameof(FirstButtonContent), typeof(object), typeof(CustomViewDataButtonsGrid), new PropertyMetadata(default(object)));
+
+        public static readonly DependencyProperty SecondButtonContentProperty = DependencyProperty.Register(
+            nameof(SecondButtonContent), typeof(object), typeof(CustomViewDataButtonsGrid), new PropertyMetadata(default(object)));
+
+        public object FirstButtonContent
+        {
+            get => (object) GetValue(FirstButtonContentProperty);
+            set => SetValue(FirstButtonContentProperty, value);
+        }
+
+        public object SecondButtonContent
+        {
+            get => (object) GetValue(SecondButtonContentProperty);
+            set => SetValue(SecondButtonContentProperty, value);
+        }
+
+        public CustomViewDataButtonsGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
